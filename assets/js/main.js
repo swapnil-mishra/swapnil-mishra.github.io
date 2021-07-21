9ecca809661318b0f5cb2406a99c15855312a4eb
SWAPNIL MISHRA <swapnilmishra2808@gmail.com>
2021-07-21 07:05:24 +0530

    Skills+Qualification added
    Final modification in details pending.

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,38 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
 
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+    for (i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if (itemClass === 'skills_content skills_close') {
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
 
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click',toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
-
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 

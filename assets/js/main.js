8d5ccb69480084262c0085a00c8206b0b32cf5f8
SWAPNIL MISHRA <swapnilmishra2808@gmail.com>
2021-07-26 05:30:30 +0530

    Content changes made

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,3 +261,23 @@ function disableCtrlKeyCombination(e) {
     }
     return true;
 }
+
+//fb chat plugin
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "105443908060349");
+chatbox.setAttribute("attribution", "biz_inbox");
+
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v11.0'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
